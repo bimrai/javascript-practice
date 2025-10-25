713b4c2fc51f1b929c7e36b5fbe095b11a0eefa3
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-10-25 17:04:02 +0100

    Update classConstructors.js
    new bank account declared and implemented/tested withdraw functionality

--- classConstructors.js
+++ classConstructors.js
@@ -9,9 +9,19 @@ class bankAccount {
         if (amt > 0) {
             this.balance += amt
             console.log(`You deposited £${amt}.`);
-            console.log(`New balance is: £${bimAccount.balance}.`);
+            console.log(`New balance is: £${this.balance}.`);
         } else {
             console.log("You cannot deposit a negative amount");
+        }
+    }
+    
+    withdraw(amt) {
+        if(amt > this.balance) {
+            console.log("Warning: Insufficient Amount")
+        } else {
+            this.balance -= amt
+            console.log(`You have withdrawn £${amt} out from your account.`)
+            console.log(`Your New Balance: £${this.balance}.`)
         }
     }
 
@@ -19,6 +29,7 @@ class bankAccount {
 
 const bimAccount = new bankAccount("123abc", "Bim Rai")
 
+// deposit test
 bimAccount.deposit(100)
 console.log(bimAccount)
 
@@ -28,6 +39,18 @@ console.log(bimAccount)
 bimAccount.deposit(-50) // testing error  
 console.log(bimAccount)
 
+// individual print test
 console.log(`Current Account: ${bimAccount.accountHolder}`)
 console.log(`Account Number: ${bimAccount.accountNumber}`)
 console.log(`Current Balance: £${bimAccount.balance}`)
+
+// withdraw test
+bimAccount.withdraw(25)
+console.log(bimAccount.balance)
+
+const anaAccount = new bankAccount("12345", "Ana De Armas")
+console.log(anaAccount)
+
+anaAccount.deposit(500);
+anaAccount.withdraw(100);
+console.log(anaAccount)
