e8178018339ac5b54c4b91463cd40a8cbf9108d4
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-10-25 16:48:15 +0100

    Create classConstructors.js
    class constructor practice

---
+++ classConstructors.js
@@ -0,0 +1,26 @@
+class bankAccount {
+    constructor(accountNumber, accountHolder, balance = 0) {
+        this.accountHolder = accountHolder;
+        this.accountNumber = accountNumber;
+        this.balance = balance;
+    }
+    
+    deposit(amt) {
+        if (amt > 0) {
+            this.balance += amt
+            console.log(`You deposited £${amt}.`);
+            console.log(`New balance is: £${bimAccount.balance}.`);
+        } else {
+            console.log("You cannot deposit a negative amount");
+        }
+    }
+
+}
+
+const bimAccount = new bankAccount("123abc", "Bim Rai")
+
+bimAccount.deposit(100)
+console.log(bimAccount)
+
+bimAccount.deposit(50)
+console.log(bimAccount)
