8bfdafbbee07c4b481dbb6c2e470c47f1d6d738a
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-11-22 19:34:02 +0000

    Implement fever check and health advice
    Add fever check logic with temperature and health advice

---
+++ feverCheck.js
@@ -0,0 +1,17 @@
+// Well well well... I have a fever... And I have not been studying the past day or two so I figured lets at least make this.
+
+let temp = 40
+
+if(temp > 37.3) {
+    console.log("You have a fever");
+} else if (temp < 37.3) {
+   console.log("You are healthy");
+}
+
+const sick = true;
+
+if(sick) {
+    console.log("Take paracetamol 1000mg every 6 hours");
+} else {
+    console.log("You are healthy");
+}
