4f255f47e56767ec14029627e6025994df1e722b
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-11-27 22:48:44 +0000

    Create infectionChecker.js

---
+++ infectionChecker.js
@@ -0,0 +1,10 @@
+// So currently dying from a throat infection and on antibiotics from today! Sucks. 
+
+let throatInfection = true;
+let fever = true
+
+if(throatInfection && fever == true) {
+    console.log("Antibiotics required!")
+} else {
+    console.log("Optimal rest needed.")
+}
