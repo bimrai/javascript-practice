c5c0cf539a0481444887fc523320ec6aa0523f3d
Bim Rai <166420374+bimrai@users.noreply.github.com>
2025-12-01 21:28:05 +0000

    Create promiseTesting.js

---
+++ promises/promiseTesting.js
@@ -0,0 +1,44 @@
+const base_URL = "https://pokeapi.co/api/v2/pokemon";
+
+fetch(`${base_URL}/1`)
+.then((res1) => {
+    console.log("Response 1", res1);
+    return fetch(`${base_URL}/2`);
+})
+.then((res2) => {
+    console.log("Response 2", res2);
+    return fetch(`${base_URL}/3`);
+})
+.then((res3) => {
+    console.log("Response 3", res3);
+    return fetch(`${base_URL}/4`);
+})
+.then((res4) => {
+    console.log("Response 4", res4)
+})
+.catch((err) => {
+    console.log("In the catch!", err)
+});
+
+
+// this will throw err
+
+fetch(`${base_URL}/1`)
+.then((res1) => {
+    console.log("Response 1", res1);
+    return fetch(`${base_URL}/2`);
+})
+.then((res2) => {
+    console.log("Response 2", res2);
+    return fetch(`${base_URL}/3`);
+})
+.then((res3) => {
+    console.log("Response 3", res3);
+    return fetch(`http://nope.nope`); // this causes the error
+})
+.then((res4) => {
+    console.log("Response 4", res4)
+})
+.catch((err) => {
+    console.log("In the catch!", err)
+});
